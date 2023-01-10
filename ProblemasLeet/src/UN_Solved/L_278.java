1cb2b9e6431f3e08170ced3d90c52b0e47dce2f3
AdriSau <107517286+AdriSau@users.noreply.github.com>
2023-01-10 22:42:25 +0100

    Update L_278.java

--- ProblemasLeet/src/UN_Solved/L_278.java
+++ ProblemasLeet/src/UN_Solved/L_278.java
@@ -1,6 +1,7 @@
 package UN_Solved;
 
 public class L_278 {
+    /*----------------------------------------------Intento 1 -----------------------------------------*/
 //    public boolean isBadVersion(int version);
 //    public int firstBadVersion(int n) {
 //        for (int i = n; i >= 0; i--) {
@@ -10,4 +11,24 @@ public class L_278 {
 //        }
 //        return 0;
 //    }
+    /*----------------------------------------------Intento 2 -----------------------------------------*/
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        return aux(0,n+1);
+//    }
+//    private int aux (int io, int n){
+//        if (io == n){
+//            return io;
+//        }else if (io +1 == n){
+//            if (isBadVersion(i)){
+//                return io;
+//            }else return n;
+//        }else{
+//            int k = (io + n) / 2;
+//            if (isBadVersion(i)){
+//                return aux(io,k);
+//            }else return  aux(k,n);
+//        }
+//    }
+//}
 }
